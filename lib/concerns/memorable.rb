9eb132bc0ea0a1f3b5c20e91f3d2b6be6e4f3b66
Laura Messa Travin <laura.travin@gmail.com>
2018-07-03 20:46:29 +0000

    automatically backed up by learn

--- lib/concerns/memorable.rb
+++ lib/concerns/memorable.rb
@@ -1,10 +1,16 @@
 
-module Memorable
-  def reset_all
-    self.all.clear
+module Memorable  #has an instance and class methods nested
+  module ClassMethods
+      def reset_all
+        self.all.clear
+      end
+      def count
+        self.all.count
+      end
+ end
+  module InstanceMethods
+      def initialize
+        self.class.all << self     #class retives the main object
+      end
   end
-  def count
-    self.all.count
-  end
-
 end
